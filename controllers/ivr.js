2eca3536e6d8a28b739e26fe1175d3b4b0be7b26
Harikrishnan V <39754@hexaware.com>
2019-09-09 16:42:11 +0530

    changes to test pro active IVR

--- controllers/ivr.js
+++ controllers/ivr.js
@@ -135,9 +135,9 @@ module.exports.getOpenOrders = function (req, res) {
 	/* create task attributes */
 	console.log(req.body);
 		var data={
-			openOrders:1,
-			date:2,
-			delivery:1
+			'openOrders':1,
+			'date':2,
+			'delivery':1
 		}
 		//data = JSON.stringify(data);
 
